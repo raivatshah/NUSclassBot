7b1653cdd0c3c17b2c475dc2eecf5f97ed79d641
Advay Pal <advaypal@hotmail.com>
2019-02-27 00:04:46 +0800

    Setup basic commands

--- Main.py
+++ Main.py
@@ -37,6 +37,13 @@ ECHO = range(0)
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SAMPLE_LIST = ['Chaitanya Baranwal', 'Raivat Shah', 'Advay Pal']
+PICKLE_FILE = 'token.pickle'
+STATE_OBJECT = {
+    "@advaypal": {
+        'token': None,
+        'session_started': False
+    }
+}
 
 ####################################
 #### Google Sheets Commands ########
@@ -50,8 +57,8 @@ def get_service():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists():
+        with open(PICKLE_FILE, 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -62,7 +69,7 @@ def get_service():
                 'credentials.json', SCOPES)
             creds = flow.run_local_server()
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(PICKLE_FILE, 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('sheets', 'v4', credentials=creds)
@@ -74,6 +81,7 @@ def create_sheet():
     """
     service = get_service()
 
+    #(TODO): Sheet might already exist
     # Call the Sheets API
     spreadsheet = {
         'properties': {
@@ -82,40 +90,103 @@ def create_sheet():
     }
     spreadsheet = service.spreadsheets().create(body=spreadsheet,
                                         fields='spreadsheetId').execute()
-    print('Spreadsheet ID: {0}'.format(spreadsheet.get('spreadsheetId')))
+    spreadsheetId = spreadsheet.get('spreadsheetId')
+    with open(PICKLE_FILE, 'w') as token:
+        pickle.dump({'id': spreadsheetId}, token)
 
-def add_values_to_sheet(time):
-    values = []
-    for name in SAMPLE_LIST:
-        values.append([name, time])
+# def add_values_to_sheet(time):
+#     values = []
+#     for name in SAMPLE_LIST:
+#         values.append([name, time])
 
-    body = {
-        'values': values
-    }
+#     body = {
+#         'values': values
+#     }
 
-    # Call the Sheets API
-    service = get_service()
-    spreadsheet = {
-        'properties': {
-            'title': 'NUSClassSample.xlsx'
-        }
-    }
-    spreadsheetId = service.spreadsheets().create(body=spreadsheet,
-                                        fields='spreadsheetId').execute().get('spreadsheetId')
+#     # Call the Sheets API
+#     service = get_service()
+#     spreadsheet = {
+#         'properties': {
+#             'title': 'NUSClassSample.xlsx'
+#         }
+#     }
+#     spreadsheetId = service.spreadsheets().create(body=spreadsheet,
+#                                         fields='spreadsheetId').execute().get('spreadsheetId')
 
-    result = service.spreadsheets().values().update(
-        spreadsheetId=spreadsheetId, range='A2:B', 
-        valueInputOption='RAW', body=body).execute()
+#     result = service.spreadsheets().values().update(
+#         spreadsheetId=spreadsheetId, range='A2:B', 
+#         valueInputOption='RAW', body=body).execute()
 
-    print('{0} cells updated.'.format(result.get('updatedCells')))
+#     print('{0} cells updated.'.format(result.get('updatedCells')))
 
 ##############################
 ##### Bot framework ##########
 ##############################
 
-def start(bot, update):
+##### Tutor ##########    
 
-    add_values_to_sheet("Now")
+def setup_sheet(bot, update):
+    username = update.message.from_user.username
+    if username not in STATE_OBJECT:
+        update.message.reply_text("Invalid command")
+        return 
+    # (TODO): Check if sheet already set up?
+    create_sheet()
+    update.message.reply_text("Sheet successfully created!")
+
+
+def generate_hash():
+    return 5
+
+def start_session(bot, update, args):
+    number_of_students = int(args[0])
+    username = update.message.from_user.username
+    if username not in STATE_OBJECT:
+        update.message.reply_text("Invalid command")
+        return
+    session_started = STATE_OBJECT[username]['session_started']
+    if session_started:
+        message = "A session is already running"
+    else:
+        STATE_OBJECT[username]['session_started'] = True
+        STATE_OBJECT[username]['session_token'] = generate_hash()
+        message = "Session Started!"
+    update.message.reply_text(message)
+
+
+def stop_session(bot, update, args):
+    number_of_students = int(args[0])
+    username = update.message.from_user.username
+    if username not in STATE_OBJECT:
+        update.message.reply_text("Invalid command")
+        return
+    session_started = STATE_OBJECT[username]['session_started']
+    if not session_started:
+        message = "No session running"
+    else:
+        STATE_OBJECT[username]['session_started'] = False
+        STATE_OBJECT[username]['session_token'] = None
+        message = "Session Stopped"
+    update.message.reply_text(message)
+
+##### Student ##########
+
+def add_value_to_sheet(name):
+    pass
+
+def indicate_attendance(bot, update, args):
+    username = update.message.from_user.username
+    if len(args) != 1:
+        update.message.reply_text("Invalid number of arguments")
+        return
+    token = int(args[0])
+    for tutor in STATE_OBJECT:
+        if tutor['session_token'] == token:
+            add_value_to_sheet(username)
+            # (TODO): Verify with student count
+            update.message.reply_text("Attendance marked!")
+            return
+    update.message.reply_text("An error occured, please validate token")
 
 def error(bot, update, error):
     """Log errors caused by updates"""
@@ -147,7 +218,11 @@ def main():
     dp = updater.dispatcher
 
     # Register different commands
-    dp.add_handler(CommandHandler('setup_sheet', start))
+    dp.add_handler(CommandHandler('setup_sheet', setup_sheet))
+    dp.add_handler(CommandHandler('start_session', start_session))
+    dp.add_handler(CommandHandler('stop_session', stop_session))
+    dp.add_handler(CommandHandler('attend', indicate_attendance))
+    # dp.add_handler(CommandHandler('help', help_func))
 
     # Register an error logger
     dp.add_error_handler(error)
